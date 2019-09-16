ba16fc401c7c74bc4237275324b8ebd78d07f814
falakjain98 <jain.falak@hotmail.com>
2019-09-16 20:53:06 +0530

    Day 8

---
+++ Day 8/Dictionaries-and-Maps.py
@@ -0,0 +1,34 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+include <map>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    map<string, int> phone_book;
+
+    for (int i = 0; i < N; i++) {
+        string name;
+        cin >> name;
+
+        if (!phone_book[name]) {
+            cin >> phone_book[name];
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        string name;
+        cin >> name;
+
+        if (phone_book[name]) {
+            cout << name << "=" << phone_book[name] << endl;
+        } else {
+            cout << "Not found" << endl;
+        }
+    }
+
+    return 0;
+}
